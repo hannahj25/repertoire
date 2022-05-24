132ba60db26304e270ee079b378fa12eec93539d
Hannah <94648556+hannahj25@users.noreply.github.com>
2022-05-24 15:03:57 +0930

    wrote User model, added typeDefs and Resolvers

---
+++ server/models/User.js
@@ -0,0 +1,59 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const validateEmail = function(email) {
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+const userSchema = new Schema(
+    {
+      username: {
+          type: String,
+          required: true,
+          unique: true,
+          trim: true
+      },
+      email: {
+          type: String,
+          required: true,
+          unique: true,
+          validate: [validateEmail, 'Please fill a valid email address.']
+  
+      },
+      password: {
+        type: String,
+        required: true,
+        minlength: 5,
+      },
+      items: [
+          {type: Schema.Types.ObjectId,
+          ref: "item"}
+      ]
+
+
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false,
+    }
+  );
+
+  userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+
+ userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+  };
+
+  const User = model('User', userSchema);
+
+module.exports = User;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,30 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+ type User {
+     _id: ID
+     username: String
+     email: String
+     password: String
+     items: []
+ }
+
+ type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]!
+    user(userId: ID!): User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+
+    removeUser(userId: ID!): User
+    
+  }
+`
+module.exports = typeDefs;
