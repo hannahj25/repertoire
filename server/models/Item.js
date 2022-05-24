db8a013bc53b1656ccef0c944ad7fe267bab8051
Hannah <94648556+hannahj25@users.noreply.github.com>
2022-05-24 15:44:22 +0930

    wrote typedefs

--- server/models/Item.js
+++ server/models/Item.js
@@ -2,7 +2,31 @@ const { Schema, model } = require('mongoose');
 
 const itemSchema = new Schema(
     {
-      
+        title: {
+            type: String,
+            required: true,
+        },
+        composer: {
+            type: String,
+            required: true,
+        },
+        genre: {
+            type: String,
+            required: true,
+        },
+        status: {
+            type: String,
+            required: true,
+
+        },
+        deadline: {
+           type: Date,
+
+        },
+        notes: {
+            type: String,
+
+        },
 
 
     },
@@ -13,7 +37,7 @@ const itemSchema = new Schema(
       id: false,
     }
   );
-  
+
 
 const Item = model('Item', itemSchema);
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,10 +3,19 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
  type User {
      _id: ID
-     username: String
-     email: String
-     password: String
-     items: []
+     username: String!
+     email: String!
+     password: String!
+     items: [Item]
+ }
+
+ type Item {
+    title: String!
+    composer: String!
+    genre: String!
+    status: String
+    deadline: Date
+    notes: String
  }
 
  type Auth {
@@ -17,14 +26,28 @@ const typeDefs = gql`
   type Query {
     users: [User]!
     user(userId: ID!): User
+
+    getItems: [Item]!
+    item(itemId: ID!): Item
+
+
   }
+
 
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
 
     removeUser(userId: ID!): User
+
+    addItem(title: String!, composer: String!, genre: String!, status: String, deadline: Date, notes: String): Item
+
+    removeItem(itemId: ID!): Item
+
+    updateItem(title: String, composer: String, genre: String, status: String, deadline: Date, notes: String): Item
     
+
+
   }
 `
 module.exports = typeDefs;
