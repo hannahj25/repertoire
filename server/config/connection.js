db7e72d4aa4062057f3cc3118ac510cf34c41c8f
Hannah <94648556+hannahj25@users.noreply.github.com>
2022-06-05 07:54:50 +0930

    updated connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/repertoire',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/repertoire',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
